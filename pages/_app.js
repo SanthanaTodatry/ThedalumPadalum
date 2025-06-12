26dfe01987c518ee187463c8d10358d5fb8906d8
Santo <51939359+SanthanaTodatry@users.noreply.github.com>
2025-06-11 19:18:37 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '../styles/globals.css'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#7c3aed" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Tamil Songs" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
